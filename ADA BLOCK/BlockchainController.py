a4c1c91ccf3daaccd8f93cbd2a960d4dc99ee549
Fidel Diaz <fidel.diaz@utp.ac.pa>
2023-12-08 23:38:44 -0500

    Comments added to the script
    #Algorithm description, input, output and comments to all functions to explain algorithm working.

--- ADA BLOCK/BlockchainController.py
+++ ADA BLOCK/BlockchainController.py
@@ -1,7 +1,13 @@
+# FINAL EXAM
+# Fidel Diaz | 3-751-509 | 1SF125 | fidel.diaz@utp.ac.pa
 import requests
 from pprint import pprint
 
 def verify_network_access_and_token():
+    # Algorithm 1: verify_network_access_and_token
+    # Verifies network access and token validity.
+    # Input: None
+    # Output: Prints messages indicating network access verification and token validity.    
     token = '89600f38d62242540816d5dd1a7a043597e23187'
     url = 'http://jsgalan.pythonanywhere.com/chain'
 
@@ -23,6 +29,10 @@ def verify_network_access_and_token():
         print(f"Token is invalid. Status code: {response.status_code}")
 
 def get_chain_state():
+    # Algorithm 2: get_chain_state
+    # Retrieves and prints the current state of the blockchain.
+    # Input: None
+    # Output: Prints the current state of the blockchain.
     url = 'http://jsgalan.pythonanywhere.com/chain'
     response = requests.get(url)
 
@@ -33,6 +43,10 @@ def get_chain_state():
         print(f"Failed to get chain state. Status code: {response.status_code}")
 
 def upload_message(sender, recipient, chat):
+    # Algorithm 3: upload_message
+    # Uploads a message to the blockchain
+    # Input: sender (str), recipient (str), chat (str)
+    # Output: Prints messages indicating successful or failed message upload.
     url = 'http://jsgalan.pythonanywhere.com/transactions/new'
     data = {'sender': sender, 'recipient': recipient, 'chat': chat}
     response = requests.post(url, json=data)
@@ -43,6 +57,10 @@ def upload_message(sender, recipient, chat):
         print(f"Failed to upload message. Status code: {response.status_code}")
 
 def mine_block(group_name, name):
+    # Algorithm 4: mine_block
+    # Mines a new block in the blockchain
+    # Input: group_name (str), name (str)
+    # Output: Prints messages indicating successful or failed block mining.
     url = 'http://jsgalan.pythonanywhere.com/mine'
     data = {'groupname': group_name, 'name': name}
     response = requests.post(url, json=data)
@@ -53,20 +71,21 @@ def mine_block(group_name, name):
         print(f"Failed to mine block. Status code: {response.status_code}")
 
 if __name__ == '__main__':
+    # Algorithm 5: Main Execution
+    # Executes the verification, retrieval, message upload, and block mining algorithms sequentially.
+    # Input: None
+    # Output: Executes the entire code, demonstrating interactions with the blockchain API.
     # Section a
-    # Verify network access and token validity
     verify_network_access_and_token()
-
     # Section b
-    # Verify blockchain state
     get_chain_state()
-
     # Section c
-    # Upload messages
-    #upload_message('Fidel Diaz', 'Jovito Guevara', 'I am mining bitcoins in blockchain!')
+    upload_message('Fidel Diaz', 'Jovito Guevara', 'These are my 4 messages for the blockchain.')
     upload_message('Fidel Diaz', 'Jovito Guevara', 'Hello computers!')
-    #upload_message('Fidel Diaz', 'Jovito Guevara', 'Lets do it again!')
-
+    upload_message('Fidel Diaz', 'Jovito Guevara', 'Lets do it again!')
+    upload_message('Fidel Diaz', 'Jovito Guevara', 'I am mining bitcoins in blockchain!')
+    upload_message('Fidel Diaz', 'Jovito Guevara', 'Hello computers!')
+    upload_message('Fidel Diaz', 'Jovito Guevara', 'Lets do it again!')
     # Section d
-    # Mine the block
     mine_block('1SF125', 'Fidel Diaz')
+# This code is contributed by ChatGPT 3.5
